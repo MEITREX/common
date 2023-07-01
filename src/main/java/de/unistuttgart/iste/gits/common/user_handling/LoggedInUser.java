dc9d3608c4fbcb3a5d62f1814e6c8a97acb71cf4
myluki2000 <lukas636@live.com>
2023-07-02 01:40:59 +0200

    Added doc comments and renamed stuff

--- src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
+++ src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
@@ -1,23 +1,24 @@
 package de.unistuttgart.iste.gits.common.user_handling;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
+/**
+ * This class represents user data for a logged-in user as provided by keycloak.
+ */
 @Getter
-public class User {
+public class LoggedInUser {
     private final UUID id;
     private final String userName;
     private final String firstName;
     private final String lastName;
 
-    public User(@JsonProperty("id") UUID id,
-                @JsonProperty("userName") String userName,
-                @JsonProperty("firstName") String firstName,
-                @JsonProperty("lastName") String lastName) {
+    public LoggedInUser(@JsonProperty("id") UUID id,
+                        @JsonProperty("userName") String userName,
+                        @JsonProperty("firstName") String firstName,
+                        @JsonProperty("lastName") String lastName) {
         this.id = id;
         this.userName = userName;
         this.firstName = firstName;
--- src/main/java/de/unistuttgart/iste/gits/common/user_handling/RequestHeaderUserProcessor.java
+++ src/main/java/de/unistuttgart/iste/gits/common/user_handling/RequestHeaderUserProcessor.java
@@ -9,12 +9,22 @@ import reactor.core.publisher.Mono;
 
 import java.util.Collections;
 
+/**
+ * Simple helper class which can be used by WebGraphQlInterceptor implementations to extract the current user from the
+ * request headers and add it to the GraphQL context.
+ */
 public class RequestHeaderUserProcessor {
+    /**
+     * Call this method from your WebGraphQlInterceptor implementation to extract the current user from the request.
+     * @param request The request to process.
+     * @param chain The chain to continue the processing.
+     * @return The response.
+     */
     @SneakyThrows
     public static Mono<WebGraphQlResponse> process(WebGraphQlRequest request, WebGraphQlInterceptor.Chain chain) {
         String value = request.getHeaders().getFirst("CurrentUser");
 
-        User currentUser = (new ObjectMapper()).readValue(value, User.class);
+        LoggedInUser currentUser = (new ObjectMapper()).readValue(value, LoggedInUser.class);
 
         request.configureExecutionInput(
                 ((executionInput, builder) -> builder.graphQLContext(Collections.singletonMap(
