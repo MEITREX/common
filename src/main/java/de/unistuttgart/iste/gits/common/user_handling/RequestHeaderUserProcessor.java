239cb5e4d426f5b907a80abc84fa94c27a11a1f1
myluki2000 <lukas636@live.com>
2023-07-02 23:38:23 +0200

    RequestHeaderUserProcessor: Modify method signature

--- src/main/java/de/unistuttgart/iste/gits/common/user_handling/RequestHeaderUserProcessor.java
+++ src/main/java/de/unistuttgart/iste/gits/common/user_handling/RequestHeaderUserProcessor.java
@@ -1,10 +1,13 @@
 package de.unistuttgart.iste.gits.common.user_handling;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.springframework.graphql.server.WebGraphQlInterceptor;
 import org.springframework.graphql.server.WebGraphQlRequest;
 import org.springframework.graphql.server.WebGraphQlResponse;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
@@ -15,23 +18,28 @@ import java.util.Collections;
  */
 public class RequestHeaderUserProcessor {
     /**
-     * Call this method from your WebGraphQlInterceptor implementation to extract the current user from the request.
+     * Call this method from your WebGraphQlInterceptor implementation to extract the current user from the request and
+     * add the user data to the graphql context as a LoggedInUser object living under the key "currentUser".
      * @param request The request to process.
-     * @param chain The chain to continue the processing.
-     * @return The response.
      */
     @SneakyThrows
-    public static Mono<WebGraphQlResponse> process(WebGraphQlRequest request, WebGraphQlInterceptor.Chain chain) {
+    public static void process(WebGraphQlRequest request) {
+        if(!request.getHeaders().containsKey("CurrentUser"))
+            return;
+
         String value = request.getHeaders().getFirst("CurrentUser");
 
-        LoggedInUser currentUser = (new ObjectMapper()).readValue(value, LoggedInUser.class);
+        try {
+            LoggedInUser currentUser = (new ObjectMapper()).readValue(value, LoggedInUser.class);
 
-        request.configureExecutionInput(
-                ((executionInput, builder) -> builder.graphQLContext(Collections.singletonMap(
-                        "currentUser", currentUser
-                )).build())
-        );
-
-        return chain.next(request);
+            request.configureExecutionInput(
+                    ((executionInput, builder) -> builder.graphQLContext(Collections.singletonMap(
+                            "currentUser", currentUser
+                    )).build())
+            );
+        } catch(JacksonException ex) {
+            // rethrow as a generic exception so we don't leak server-internal data to the client (json contents)
+            throw new IllegalArgumentException("Invalid user data in request header.");
+        }
     }
 }
