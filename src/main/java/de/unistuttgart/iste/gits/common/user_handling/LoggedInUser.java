7bcb70b90c998c576708d3f76cf702ba840ec6c3
Paul Bredl <34026908+PaulBredl@users.noreply.github.com>
2023-09-11 18:37:55 +0200

    remove resource markdown (#18)
    * remove resource markdown
    
    * revert LoggedInUser
    
    * remove test

--- src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
+++ src/main/java/de/unistuttgart/iste/gits/common/user_handling/LoggedInUser.java
@@ -1,6 +1,5 @@
 package de.unistuttgart.iste.gits.common.user_handling;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
