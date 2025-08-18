0ca92bcade904d4a20b9aa10d2f066c9e605c712
myluki2000 <lukas636@live.com>
2025-08-18 15:27:12 +0200

    Add GraphQlScalarsAndValidationConfiguration

---
+++ src/main/java/de/unistuttgart/iste/meitrex/common/config/AutoConfigure.java
@@ -0,0 +1,10 @@
+package de.unistuttgart.iste.meitrex.common.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan
+public class AutoConfigure {
+    // intentionally left empty
+}
