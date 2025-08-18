481e664801fdc578d5452abe10b01bc623a36477
myluki2000 <lukas636@live.com>
2025-08-18 17:56:33 +0200

    TopicPublisherConfiguration remove prod constraint

--- src/main/java/de/unistuttgart/iste/meitrex/common/config/TopicPublisherConfiguration.java
+++ src/main/java/de/unistuttgart/iste/meitrex/common/config/TopicPublisherConfiguration.java
@@ -4,10 +4,8 @@ import de.unistuttgart.iste.meitrex.common.dapr.TopicPublisher;
 import io.dapr.client.DaprClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod")
 public class TopicPublisherConfiguration {
     @Bean
     public TopicPublisher topicPublisher() {
